29ae3b45472c61d98f697fd0100d463b08de23df
DiegoBas17 <167855623+DiegoBas17@users.noreply.github.com>
2024-07-07 13:02:27 +0200

    add component for second page

--- src/assets/newComponent/ForecastMeteoCard.jsx
+++ src/assets/newComponent/ForecastMeteoCard.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import Slider from "react-slick";
 
 const ForecastMeteoCard = (props) => {
   const [forecast, setForecast] = useState("");
@@ -21,6 +23,102 @@ const ForecastMeteoCard = (props) => {
   useEffect(() => {
     fetchForecast5d3h();
   }, []);
-  return <div>ciao</div>;
+
+  const getWeatherIconUrl = (iconCode) => {
+    return `http://openweathermap.org/img/w/${iconCode}.png`;
+  };
+
+  const calcolaOraEsatta = (timestampUNIX) => {
+    const data = new Date(timestampUNIX * 1000);
+
+    const ora = data.getHours().toString().padStart(2, "0");
+    const minuti = data.getMinutes().toString().padStart(2, "0");
+
+    const oraFormattata = `${ora}:${minuti}`;
+
+    return oraFormattata;
+  };
+  const calcolaGiornoSettimana = (timestampUNIX) => {
+    const data = new Date(timestampUNIX * 1000);
+    const giorniSettimana = [
+      "Domenica",
+      "Lunedì",
+      "Martedì",
+      "Mercoledì",
+      "Giovedì",
+      "Venerdì",
+      "Sabato",
+    ];
+
+    return giorniSettimana[data.getDay()];
+  };
+  const calcolaGiornoEMese = (timestampUNIX) => {
+    const data = new Date(timestampUNIX * 1000);
+    const mesi = [
+      "Gennaio",
+      "Febbraio",
+      "Marzo",
+      "Aprile",
+      "Maggio",
+      "Giugno",
+      "Luglio",
+      "Agosto",
+      "Settembre",
+      "Ottobre",
+      "Novembre",
+      "Dicembre",
+    ];
+
+    const giornoMese = data.getDate().toString().padStart(2, "0");
+    const mese = mesi[data.getMonth()];
+
+    return `${giornoMese}/${mese}`;
+  };
+
+  const kelvinToCelsius = (kelvin) => {
+    return Math.floor(kelvin - 273.15);
+  };
+
+  /* slick verticale */
+  const settings = {
+    dots: false,
+    infinite: false,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    vertical: true,
+    verticalSwiping: true,
+    centerPadding: "60px",
+    swipeToSlide: true,
+  };
+  return (
+    <Container>
+      <h2>Nelle prossime ore:</h2>
+      <div className="slider-container">
+        <Slider {...settings}>
+          {forecast &&
+            forecast.map((meteo, index) => (
+              <div key={index}>
+                <Row className="text-center align-items-center">
+                  <Col md="3">
+                    <img
+                      src={getWeatherIconUrl(meteo.weather[0].icon)}
+                      alt=""
+                      className="d-inline"
+                    />
+                    <h3 className="d-inline">
+                      +{kelvinToCelsius(meteo.main.temp_max)}/ +
+                      {kelvinToCelsius(meteo.main.temp_min)}
+                    </h3>
+                  </Col>
+                  <Col md="3">{calcolaOraEsatta(meteo.dt)} </Col>
+                  <Col md="3">{calcolaGiornoSettimana(meteo.dt)}</Col>
+                  <Col md="3">{calcolaGiornoEMese(meteo.dt)}</Col>
+                </Row>
+              </div>
+            ))}
+        </Slider>
+      </div>
+    </Container>
+  );
 };
 export default ForecastMeteoCard;
